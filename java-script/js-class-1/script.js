4557c94223e3acc686410767020bb30c7eef7d1f
prashant <prashant @ ronijdaikomailserver dot com>
2026-03-25 09:44:44 +0545

    js-assignment-4

---
+++ java-script/js-class-1/script.js
@@ -0,0 +1,21 @@
+console.log('Hello World');
+
+function addNumbers(x, y) {
+  return x + y;
+}
+
+let a = 5;
+let b = 6;
+let c = 7;
+
+console.log('The sum is:', addNumbers(a, b));
+console.log('The sum2 is:', addNumbers(b, c));
+console.log('The sum3 is:', addNumbers(a, c));
+
+var x = 25;
+var y = 4;
+var m = x * y;
+var remainder = x % y; 
+
+console.log('The multiply  is:', m);
+console.log('The remainder is:', remainder); 
